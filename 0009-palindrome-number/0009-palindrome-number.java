fd9bbeaef5cf0909d7847fd3dea69c96c35f60a9
karthick45013 <karthick45013@gmail.com>
2026-07-31 03:30:42 +0530

    Time: 5 ms (80.93%), Space: 43.8 MB (99.99%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        long sum=0;
+        int a=x;
+        while(x!=0){
+            int n=x%10;
+            sum=sum*10+n;
+            x=x/10;
+        }
+        if(a<0)
+        {
+            return false;
+        }
+        if(a==sum)
+        {
+            return true;
+        }
+        return false;
+    }}
